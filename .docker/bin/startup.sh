13ac006eb8b075282c5c126ae60f9b1a25a1014e
criskell <96352451+criskell@users.noreply.github.com>
2023-07-18 20:14:55 +0000

    fixes

--- .docker/bin/startup.sh
+++ .docker/bin/startup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 if [ ! -s web/bin/magento ]; then
+    composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition web
+
     installerPath=web/setup/src/Magento/Setup/Model/Installer.php
 
     # This checks takes all of my RAM.
@@ -8,7 +10,11 @@ if [ ! -s web/bin/magento ]; then
     sed -i "/\s*\$script\[\] = \['File permissions check...', 'checkInstallationFilePermissions', \[\]\];/d" $installerPath
     sed -i "/\s*\$script\[\] = \['Required extensions check...', 'checkExtensions', \[\]\];/d" $installerPath
 
-    web/bin/magento setup:install --base-url=http://0.0.0.0:80/ \
+    bin/magento module:status | grep -v Magento | grep -v List | grep -v None | grep -v -e '^$'| xargs php bin/magento module:disable
+    bin/magento cache:clean
+    bin/magento setup:upgrade
+
+    web/bin/magento setup:install --base-url=http://0.0.0.0:8080/ \
     --db-host=mysql --db-name=magento \
     --db-user=magento --db-password=magento \
     --admin-firstname=Magento --admin-lastname=Magento --admin-email=magento@example.com \
